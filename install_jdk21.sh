db47e2147913c4b4d68b2b8504e81d09fcfb59bd
zryyoung <59283369+zryyoung@users.noreply.github.com>
2024-03-18 01:47:11 +0800

    Add files via upload

--- install_jdk21.sh
+++ install_jdk21.sh
@@ -16,14 +16,14 @@ elif ! [ -f openjdk-21.0.1-aarch64.zip   ]; then
 	wget https://github.com/zryyoung/openjdk-Termux/releases/download/openjdk-21.0.1/openjdk-21.0.1-aarch64.zip -O openjdk-21.0.1-aarch64.zip &&
 	echo "下载完成  正在解压" 
 fi
-unzip openjdk-21.0.1-aarch64.zip &&
+unzip openjdk-21.0.1-aarch64.zip -d /data/data/com.termux/files/usr/opt/ &&
 rm -rf openjdk-21.0.1-aarch64.zip &&
-if [ -f "$HOME/.bashrc" ]; then
-    echo "export JAVA_HOME=~/21.0.1" >> $HOME/.bashrc
-    echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> $HOME/.bashrc
+if [ -f "/data/data/com.termux/files/usr/etc/bash.bashrc" ]; then
+    echo "export JAVA_HOME=/data/data/com.termux/files/usr/opt/openjdk-21.0.1" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+    echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 else
-    touch $HOME/.bashrc
-    echo "export JAVA_HOME=~/openjdk-21.0.1" >> $HOME/.bashrc
-    echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> $HOME/.bashrc
+    touch /data/data/com.termux/files/usr/etc/bash.bashrc
+    echo "export JAVA_HOME=/data/data/com.termux/files/usr/opt/openjdk-21.0.1" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+    echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 fi
 echo "环境变量配置完成，请重启终端更新配置"
